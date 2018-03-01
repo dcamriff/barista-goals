a723280eae3c5863d3d1281e20a4fa84cec1d216
dcamriff <driffelatl@gmail.com>
2018-02-28 19:06:58 -0500

    Added api controllers for users, categories, field_notes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    get 'users/retrieve/:username', to: 'users#retrieve'
+    resources :users do
+      resources :field_notes
+    end
+    resources :categories do 
+      resources :field_notes
+    end
+  end
 end
