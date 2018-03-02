7b4dbeeb6792df0c4ec73a850fedabc4a55227fc
dcamriff <driffelatl@gmail.com>
2018-03-01 23:17:47 -0500

    Showing username with field_note

--- client/src/components/FieldNote.js
+++ client/src/components/FieldNote.js
@@ -3,14 +3,15 @@ import { Link } from 'react-router-dom'
 import { FieldNotesContainer, FieldNoteSingleContainer } from './styled-components/Containers'
 
 const FieldNote = (props) => {
-
     return (
         <FieldNotesContainer>
             <FieldNoteSingleContainer>
                 <img src={props.image} alt={props.title}/>
             </FieldNoteSingleContainer>
             <div>
+                <h1>{props.user.username}</h1>
                 <h3>{props.title}</h3>
+                <p>{props.user_id}</p>
                 <p>{props.memo}</p>
                 <p>{props.year}</p>
                 <p>{props.tags}</p>
